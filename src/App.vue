159a761f4b48a82a2af708d34dda65b510dd11bf
左浩辰 <3017218182@tju.edu.cn>
2021-03-28 18:33:27 +0800

    调整路由，实现嵌套路由（router.js）： 登录组件（login） 编写者界面（writer）： 	编辑界面（edit.vue） 	草稿箱（draft.vue） 	历史记录（history.vue） 	审核进度（waiting.vue） 	账号信息（setting.vue）： 		编号 		身份 		电话 		姓名 		密码 审核员界面（reviewer）: 	待审核 	历史记录     账号信息

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,10 @@
 <template>
   <div id="app">
     <el-container>
-      <el-header>管理系统</el-header>
-      <el-container>
-        <el-aside width="250px">
-          <el-menu default-active="2" :router="true">
-            <el-menu-item index="1" route="/edit">
-              <span slot="title">新建文章</span>
-            </el-menu-item>
-            <el-menu-item index="2" route="/waiting">
-              <span slot="title">审核结果</span>
-            </el-menu-item>
-            <el-menu-item index="3" route="/history">
-              <span slot="title">历史记录</span>
-            </el-menu-item>
-            <el-menu-item index="4" route="/draft">
-              <span slot="title">草稿箱</span>
-            </el-menu-item>
-            <el-menu-item index="5">
-              <span slot="title">账号设置</span>
-            </el-menu-item>
-          </el-menu>
-        </el-aside>
-        <el-main >
-          <router-view></router-view>
-        </el-main>
-      </el-container>
+      <el-header style="background-color: #42b983">管理系统</el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
 <!--    <el-row>-->
 <!--      <el-col span="6">-->
@@ -57,6 +36,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -2,31 +2,81 @@ import Waiting from "./components/waiting"
 import History from "./components/history"
 import Draft from "./components/draft"
 import Edit from "./components/edit"
+import Writer from "./pages/writer"
+import Login from "./pages/login"
+import Reviewer from "./pages/reviewer"
+import Info from "./components/info"
+import Toview from "./components/toview"
+import Viewed from "./components/viewed"
 
 const routers = [
     {
-        path : '/waiting',
-        name : 'waiting',
-        component : Waiting
+        path : '/writer',
+        name : 'writer',
+        component : Writer,
+        children : [
+            {
+                path : 'waiting',
+                name : 'waiting',
+                component : Waiting
+            },
+            {
+                path : '',
+                component : Waiting
+            },
+            {
+                path : 'draft',
+                name : 'draft',
+                component : Draft
+            },
+            {
+                path : 'edit',
+                name : 'edit',
+                component : Edit
+            },
+            {
+                path : 'history',
+                name : 'history',
+                component: History
+            },
+            {
+                path: 'info',
+                name: 'info',
+                component: Info
+            }
+        ]
     },
     {
-        path : '/',
-        component : Waiting
+        path : '',
+        name : 'login',
+        component : Login
     },
     {
-        path : '/draft',
-        name : 'draft',
-        component : Draft
-    },
-    {
-        path : '/edit',
-        name : 'edit',
-        component : Edit
-    },
-    {
-        path : '/history',
-        name : 'history',
-        component: History
+        path : '/reviewer',
+        name : 'reviewer',
+        component : Reviewer,
+        children : [
+            {
+                path: '',
+                name: 'toview',
+                component: Toview
+            },
+            {
+                path: 'toview',
+                name: 'toview',
+                component: Toview
+            },
+            {
+                path: 'viewed',
+                name: 'viewed',
+                component: Viewed
+            },
+            {
+                path: 'info',
+                name: 'info',
+                component: Info
+            }
+        ]
     }
 ]
 export default routers
